20da7fcb23f93a12bdad4d0c31e7fe6ba797d726
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-08-03 13:33:16 +0800

    fix(build): apply patch to errors

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,14 @@
+import { Command } from '../../models/command/BaseCommand';
+import { MessageOptions } from 'discord.js';
+
+export default new Command(
+	async ({ msg }): Promise<MessageOptions> => ({
+		embeds: [{
+			description: `**API Latency:** \`${Math.round(msg.guild.shard.ping)}ms\``
+		}]
+	}),
+	{
+		name: 'ping',
+		aliases: ['ping', 'pong'],
+	}
+);
--- src/models/command/OwnerCommand.ts
+++ src/models/command/OwnerCommand.ts
@@ -1,11 +1,11 @@
 import { Command, CommandParams } from './BaseCommand';
 
 export class OwnerCommand extends Command {
-  public async execute(args: CommandParams) {
+  public execute(args: CommandParams) {
     if (!args.ctx.config.owners.includes(args.msg.author.id)) return;
 
     try {
-      return await super.execute(args);
+      return super.execute(args);
     } catch (e) {
       console.error(e.stack);
     }
