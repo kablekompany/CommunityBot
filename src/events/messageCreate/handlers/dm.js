9b8e6a774f65d308507b37ed247b3cc85aab9bda
Dauntless7 <47513707+Dauntless7@users.noreply.github.com>
2021-10-04 02:20:37 -0400

    fix dm logger

--- src/events/messageCreate/handlers/dm.js
+++ src/events/messageCreate/handlers/dm.js
@@ -2,7 +2,7 @@ const MessageHandler = require('../../../models/Handlers/MessageHandler');
 
 module.exports = new MessageHandler(
   ({ ctx, msg }) => {
-    if (msg.channel.type !== 'dm') return null;
+    if (msg.channel.type !== 'DM') return null;
     const { dmLog } = ctx.config.log;
     if (dmLog.enabled) {
       ctx.utils.logmsg(ctx.bot, msg, dmLog.channel);
