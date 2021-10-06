00df9d39d78dcc0ff7c556b207d690ef8122440f
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-10-06 09:44:12 +0800

    feat[*]: delete old handlers

--- src/handlers/index.ts
+++
@@ -1,6 +0,0 @@
-import { readdirSync } from 'fs';
-import { join } from 'path';
-
-export default readdirSync(__dirname)
-  .filter((l) => l !== 'index.js')
-  .map((l) => require(join(__dirname, l)).default);
