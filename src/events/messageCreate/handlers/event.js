dfe512a9ecc0ff98f4d6e769a597631360ca7385
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-10-31 18:07:24 +0800

    add filters for #event-entry (req. by aiph)

---
+++ src/events/messageCreate/handlers/event.js
@@ -0,0 +1,20 @@
+const MessageHandler = require('../../../models/Handlers/MessageHandler');
+
+module.exports = new MessageHandler(
+  async ({ ctx, msg }) => {
+    if (msg.channel.id !== '847656883401129995') {
+      return null;
+    }
+
+    if (!msg.content.toLowerCase().match(/^pls (share|gift|give|yeet)/g)) {
+      await msg.delete();
+    }
+
+    return null;
+  },
+  {
+    name: 'event',
+    allowDM: false,
+    allowBot: true,
+  },
+);
