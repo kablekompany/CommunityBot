04f5ced26b3aa197150e0860617d21c5e36c7230
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-11-27 22:00:34 +0800

    /commands ig

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import { CommunityBot } from '#dmc/client';
+import { container, SapphireClient } from '@sapphire/framework';
+import { type Config, config } from '#dmc/config';
+import { Database } from '#dmc/db';
+import { Player } from 'discord-player';
 import { join } from 'path';
 
-const dmc = new CommunityBot({ 
+declare module '@sapphire/pieces' {
+	interface Container {
+		config: Config;
+		db: Database;
+		player: Player;
+	}
+}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DISCORD_TOKEN: string;
+    }
+  }
+}
+
+
+const dmc = new SapphireClient({ 
 	baseUserDirectory: join(process.cwd(), 'src'),
 	intents: 5711,
 });
+
+container.config = config;
+container.db = new Database(dmc);
+container.player = new Player(dmc);
 
 await dmc.login();
--- src/library/structures/Slash.ts
+++ src/library/structures/Slash.ts
@@ -1,8 +1,17 @@
+import type { ApplicationCommandData, Awaitable, CommandInteraction } from 'discord.js';
 import type { PieceContext, PieceOptions } from '@sapphire/framework';
 import { Piece, Store } from '@sapphire/framework';
 
-export class Slash extends Piece {}
+export class Slash extends Piece<Slash.Options> {
+	public data: ApplicationCommandData;
+	public constructor(ctx: PieceContext, options: Slash.Options) {
+		super(ctx, options);
+		this.data = options.data;
+	}
+}
 
 export namespace Slash {
-	export interface Options extends PieceOptions {}
+	export interface Options extends PieceOptions {
+		data: ApplicationCommandData;
+	}
 }
