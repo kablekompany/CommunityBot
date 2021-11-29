f30ce237bf15d9ee2938653d8530a014e093e5d4
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2021-11-29 12:06:49 +0800

    'reload' owner command

---
+++ src/arguments/store.ts
@@ -0,0 +1,19 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Argument } from '@sapphire/framework';
+
+import type { StoreRegistryEntries } from '@sapphire/pieces';
+import { isNullOrUndefined } from '@sapphire/utilities';
+import { type Guild } from 'discord.js';
+
+export default class extends Argument {
+  public async run(parameter: string, ctx: Argument.Context) {
+    const store = this.container.stores.get(parameter);
+    return store ? this.ok(store) : this.error({ parameter, message: 'Unknown store.' });
+  }
+}
+
+declare module '@sapphire/framework' {
+  interface ArgType {
+    store: StoreRegistryEntries[keyof StoreRegistryEntries];
+  }
+}
--- src/commands/owner/reload.ts
+++ src/commands/owner/reload.ts
@@ -2,10 +2,25 @@ import { type Args, Command } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 import { type Message } from 'discord.js';
 
+import { Formatters } from 'discord.js';
+
+const { inlineCode: ic } = Formatters;
+
 @ApplyOptions<Command.Options>({
   aliases: ['reload'],
   preconditions: ['OwnerOnly'],
 })
 export default class extends Command {
-  public messageRun(msg: Message, args: Args) {}
+  public async messageRun(msg: Message, args: Args) {
+    const store = await args.pickResult('store');
+    if (store.error) {
+      return msg.reply(`
+        These are the only available stores:
+        ${ic(this.container.stores.map((_, k) => k).join(ic(', ')))}
+      `);
+    }
+
+    await store.value.loadAll();
+    await msg.reply(`Reloaded the ${ic(store.value.Constructor.name.toLowerCase())} store.`);
+  }
 }
